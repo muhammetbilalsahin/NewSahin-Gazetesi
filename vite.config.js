5cdd5458739070adc91ef87a6ad50e449a216748
sahinbilisim <shnblsm@gmail.com>
2025-05-30 14:13:31 +0300

    vite

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/NewSahin-Gazetesi",
-  plugins: [vanilla()],
+
   server: {
     open: true, // Proje çalışınca tarayıcıda otomatik açılır
     port: 3000, // Geliştirme sunucusu portu
